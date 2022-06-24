0949c26cb1f9cd1e1ef0d1c6e252bbdfa6f69ff6
Faith A <faithta90@gmail.com>
2022-06-24 13:46:57 -0700

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+////////////////////////////
+// Javascript for Posts page
+////////////////////////////
+
+$(function() {
+    // Executed when js-menu-icon js clicked
+    $('.js-menu-icon').click(function(){
+        $(this).next().toggle();
+    })
+})
